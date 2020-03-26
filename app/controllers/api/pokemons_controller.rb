362516726a13cae45de30d687baa8109d98710c0
Nick Semerad <nicksemerad@gmail.com>
2020-03-25 19:37:56 -0600

    navbar

---
+++ app/controllers/api/pokemons_controller.rb
@@ -0,0 +1,36 @@
+class Api::PokemonsController < ApplicationController
+
+  def index
+    render json: Pokemon.all
+  end
+
+  def create
+    @pokemon = Pokemon.new(poke_params)
+    if @pokemon.save
+      render json: @pokemon
+    else
+      render json: { errors: @pokemon.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @pokemon = Pokemon.find(params[:id])
+    if @pokemon.update(poke_parms)
+      render json: @pokemon
+    else
+      render json: { errors: @pokemon.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Pokemon.find(params[:id]).destroy
+    render json: { message: 'Pokemon released'}
+  end
+
+  private
+
+  def poke_params
+    params.require(:pokemon).permit(:name, :type, :level, :location, :move)
+  end
+
+end
